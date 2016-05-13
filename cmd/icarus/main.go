2c38600176a533bf1c8cb33207053b5b96dba81a
Will Larson <lethain@gmail.com>
2016-05-13 08:17:56 -0700

    Add basic template rendering, with composition.

--- cmd/icarus/main.go
+++ cmd/icarus/main.go
@@ -8,9 +8,10 @@ import (
 )
 
 var loc = flag.String("loc", ":8080", "host:port to bind to, defaults to :8080")
+var templates = flag.String("templates", "templates/", "directory holding layouts/ and includes/ directories for templates")
 
 func main() {
 	flag.Parse()
 	fmt.Printf("Starting Icarus on %v.\n", *loc)
-	icarus.Serve(*loc)
+	icarus.Serve(*loc, *templates)
 }
--- server.go
+++ server.go
@@ -2,20 +2,64 @@ package icarus
 
 import (
 	"fmt"
+	"path/filepath"
+	"log"
 	"net/http"
+	"html/template"
 )
 
-func handleBase(w http.ResponseWriter, r *http.Request) {
+var templateCache map[string]*template.Template
+
+// Handle rendering generic pages stored in Redis.
+func handlePage(w http.ResponseWriter, r *http.Request) {
 	page, err := PageFromRedis(r.URL.Path[1:])
 	if err != nil {
 		fmt.Fprintf(w, "Error opening slug '%s'\n%v\n\n%v", r.URL.Path[1:], err, page)
-	} else {
-		fmt.Fprintf(w, "Hi, there, you're at slug '%s'\n\n%v", r.URL.Path[1:], page)
+		return
 	}
 
+	params := make(map[string]interface{})
+	params["Page"] = page
+	err = renderTemplate(w, "page.html", params)
+	if err != nil {
+		log.Printf("error rendering: %v", err)
+	}
 }
 
-func Serve(loc string) {
-	http.HandleFunc("/", handleBase)
+func Serve(loc string, templatePath string) {
+	loadTemplates(templatePath)
+	http.HandleFunc("/", handlePage)
 	http.ListenAndServe(loc, nil)
+}
+
+func renderTemplate(w http.ResponseWriter, name string, data map[string]interface{}) error {
+	tmpl, ok := templateCache[name]
+	if !ok {
+		return fmt.Errorf("template %s does not exist", name)
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return tmpl.ExecuteTemplate(w, "base", data)
+}
+
+func loadTemplates(templatePath string) {
+	if templateCache == nil {
+		templateCache = make(map[string]*template.Template)
+	}
+
+	layouts, err := filepath.Glob(templatePath + "layouts/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	includes, err := filepath.Glob(templatePath + "includes/*.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, layout := range layouts {
+		files := append(includes, layout)
+		log.Printf("loading and composing templates for %v : %v\n", filepath.Base(layout), files)
+		
+		templateCache[filepath.Base(layout)] = template.Must(template.ParseFiles(files...))
+	}
 }
